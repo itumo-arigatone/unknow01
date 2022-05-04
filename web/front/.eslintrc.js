d23e6f1268728bd79852e08c5612e7d7cdb92186
unkomorasi01 <soganokasegi0825@gmail.com>
2022-05-04 13:53:52 +0900

    react

---
+++ web/front/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"],
+    "prettier/prettier": "error",
+  },
+  settings: {
+    react: {
+        version: "detect",
+    },
+  }
+}
