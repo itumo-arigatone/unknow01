a531cdd739d6cb3b30a3c51c3f5dae69dba29bf2
unkomorasi01 <soganokasegi0825@gmail.com>
2022-05-06 04:25:37 +0900

    ホットリロードがされないバグ対応

---
+++ web/front/setup.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+// ホットリロードができるようにするスクリプト↓参考
+// https://qiita.com/snorkel/items/f78911b25c3174732c70
+// https://github.com/facebook/create-react-app/issues/11879
+
+// if (process.env.NODE_ENV === 'development') {
+  const webPackConfigFile = path.resolve('./node_modules/react-scripts/config/webpack.config.js');
+  let webPackConfigFileText = fs.readFileSync(webPackConfigFile, 'utf8');
+
+  if (!webPackConfigFileText.includes('watchOptions')) {
+    if (webPackConfigFileText.includes('performance: false,')) {
+      webPackConfigFileText = webPackConfigFileText.replace(
+        'performance: false,',
+        "performance: false,\n\t\twatchOptions: { aggregateTimeout: 200, poll: 1000, ignored: '**/node_modules', },"
+      );
+      fs.writeFileSync(webPackConfigFile, webPackConfigFileText, 'utf8');
+    } else {
+      throw new Error(`Failed to inject watchOptions`);
+    }
+  }
+// }
